e0312c237933f4a80f3de5af3b5208aadc5d4bff
= <=>
2023-05-16 14:26:34 +0700

    revisi satu

--- application/views/index/welcome.php
+++ application/views/index/welcome.php
@@ -18,8 +18,8 @@
                     <div class="divider-custom-line"></div>
                 </div>
                 <div class="row">
-                    <div class="col-lg-4 ms-auto"><p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
-                    <div class="col-lg-4 me-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+                    <div class="col-lg-4 ms-auto"><p class="lead">WPFutsal adalah tempat anda dapat menyalurkan hobi bermain futsal</p></div>
+                    <div class="col-lg-4 me-auto"><p class="lead">Dengan memesan via Web anda dapat mempermudah pemesanan</p></div>
                 </div>
                 <div class="text-center mt-4">
                     <?php if ($this->session->userdata('role_id') == 1) { ?>
